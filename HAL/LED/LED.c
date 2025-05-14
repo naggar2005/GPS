8b9c8ffed35455d10661f3e009cf9f7349929e19
ahmed-mo690 <ahmed.mohamed18004@gmail.com>
2025-05-14 22:58:29 +0300

    Add files via upload

---
+++ HAL/LED/LED.c
@@ -0,0 +1,66 @@
+#include "../../LIB/TM4C123GH6PM.h"
+#include "../../MCAL/GPIO/GPIO.h"
+#include "LED.h"
+#include "../../MCAL/systick/SysTick.h"
+void LED_LedInit(void)
+{
+  GPIO_Init(GPIO_PORTF);
+	//GPIO_Pin_Init(GPIO_PORTF,LED_RED_PIN);
+	//GPIO_Pin_Init(GPIO_PORTF,LED_BLUE_PIN);
+	//GPIO_Pin_Init(GPIO_PORTF,LED_GREEN_PIN);
+	set_Pin_Direction(GPIO_PORTF, LED_BLUE_PIN, GPIO_OUTPUT);   /**< then initialize the LED direction */
+	set_Pin_Direction(GPIO_PORTF, LED_GREEN_PIN, GPIO_OUTPUT);  /**< then initialize the LED direction */  
+	set_Pin_Direction(GPIO_PORTF, LED_RED_PIN, GPIO_OUTPUT);    /**< then initialize the LED direction */
+}
+
+void LED_LedOn(u8 LEDColour)
+{
+    switch(LEDColour)
+    {
+        case LED_BLUE:
+            set_Pin_Value(GPIO_PORTF, LED_BLUE_PIN, GPIO_HIGH);
+            break;
+        case LED_GREEN:
+            set_Pin_Value(GPIO_PORTF, LED_GREEN_PIN, GPIO_HIGH);
+            break;
+        case LED_RED:
+            set_Pin_Value(GPIO_PORTF, LED_RED_PIN, GPIO_HIGH);
+            break;
+        default :/**< Error State */ break;
+    }
+}
+
+void LED_vLedOff(u8 LedColour)
+{
+    switch(LedColour)
+    {
+        case LED_BLUE:
+            set_Pin_Value(GPIO_PORTF, LED_BLUE_PIN, GPIO_LOW);
+            break;
+        case LED_GREEN:
+            set_Pin_Value(GPIO_PORTF, LED_GREEN_PIN, GPIO_LOW);
+            break;
+        case LED_RED:
+            set_Pin_Value(GPIO_PORTF, LED_RED_PIN, GPIO_LOW);
+            break;
+        default :/**< Error State */ break;
+    }
+}
+
+void LED_LedOffAll(void)
+{
+	set_Pin_Value(GPIO_PORTF, LED_BLUE_PIN, GPIO_LOW);
+	set_Pin_Value(GPIO_PORTF, LED_RED_PIN, GPIO_LOW);
+	set_Pin_Value(GPIO_PORTF, LED_GREEN_PIN, GPIO_LOW);
+}
+
+void LED_OUTPUT(float DISTANCE){
+	if(DISTANCE < 5) {
+            LED_LedOn(LED_GREEN);
+        } else if(DISTANCE < 15) {
+            LED_LedOn(LED_BLUE);
+        } else {
+            LED_LedOn(LED_RED);
+        }
+}
+
---
+++ HAL/LED/LED.h
@@ -0,0 +1,45 @@
+#include "../../LIB/TYPE_DEF.h"
+
+/********************************< MACROS for colours ********************************/
+#define LED_RED  	 	1
+#define LED_BLUE 		2
+#define LED_GREEN	    3
+
+/**********************************< _SELECT_MC_PIN_ ***************************************/
+#define LED_RED_PIN  	 	1
+#define LED_BLUE_PIN 		2
+#define LED_GREEN_PIN	    3
+/**********************************< _SELECT_LED_TIME_ON_ **********************************/
+#define LED_TIME_ON			100000 // (25 ms)
+
+/********************************< BONUS DESCRIPTION *********************************/
+
+/* 
+
+Green LED turns on when the target destination is reached.
+BLUE LED turns on when the target destination is about to be reached (less than 5 meters away).
+Red LED turns on when the target destination is far away (more than 5 meters).
+ 
+*/ 
+
+/********************************< functions for LEDs *********************************/
+
+/* Initializes LED pins as output by configuring the relevant GPIO pins.
+
+
+ */ 
+void LED_LedInit(void);
+
+/*	Turns on the specified LED (Red, Green, or Blue) by setting its pin HIGH.*/
+
+void LED_LedOn(u8 LedColour);	 
+
+/*	Turns off the specified LED by setting its pin LOW.*/
+
+void LED_LedOff(u8 LedColour);
+
+/*	Turns off all LEDs by setting their corresponding pins LOW.*/
+
+void LED_LedOffAll(void);
+
+void LED_OUTPUT(float DISTANCE);
